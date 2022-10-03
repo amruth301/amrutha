8f45bc313a5e761375efb8e2149b4c589f091ab8
sumat-qa <92343711+sumat-qa@users.noreply.github.com>
2022-10-03 08:14:57 -0400

    Update and rename LongestSubString.java to Parantheses.java

---
+++ src/main/java/com/test/find/Parantheses.java
@@ -0,0 +1,22 @@
+package com.test.find;
+
+import java.util.List;
+
+/**
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+* Open brackets must be closed by the same type of brackets.
+* Open brackets must be closed in the correct order.
+* Every close bracket has a corresponding open bracket of the same type.
+ */
+
+public class Parantheses {
+
+    public boolean validParatheses(String input){
+        boolean flag = false
+        //Put your code here and return expected results
+
+        return flag;
+    }
+}
